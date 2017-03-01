bcec116e6a834720789c9260cce39f51c3a8c3a9
efung001 <efung001@ucr.edu>
2017-02-28 23:50:56 -0800

    rshell

---
+++ src/command.h
@@ -0,0 +1,30 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+
+    
+class Command
+{
+    private:
+        std::string cmd;
+    public:
+        Command(); 
+        Command(std::string a); 
+        ~Command();
+        std::string getString();
+        int run(); // return -1 if not work
+};
+
+#endif
